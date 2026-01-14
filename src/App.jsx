071fec1fbd8509255161f55bfb5bc4ea08c1c3d3
arioseno265 <arioseno2605@gmail.com>
2026-01-14 20:14:54 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { BrowserRouter, Routes, Route, NavLink, Navigate } from "react-router-do
 import { useEffect, useState } from "react";
 
 // pages
-import Home from "./pages/Home";
-import Todo from "./pages/Todo";
-import Users from "./pages/Users";
+import Home from "./pages/home";
+import Todo from "./pages/todo";
+import Users from "./pages/users";
 
 function App() {
   // theme (localStorage)
